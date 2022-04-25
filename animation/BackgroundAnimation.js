358f9ec07150b7f2c856659c638956365794878d
Christopher Rhodes <crhodes@amazon.com>
2022-04-24 23:39:37 -0700

    Moved background animation into a separate class

---
+++ animation/BackgroundAnimation.js
@@ -0,0 +1,170 @@
+import {BACKGROUND_WIDTH_PX, LEFT, PPS, RIGHT, SCROLLING_ACTIONS} from "../util/constants";
+import {Animated, Easing} from "react-native";
+
+export class BackgroundAnimation {
+    constructor(builder) {
+        this._backgroundInfo = builder.backgroundInfo;
+        this._backgroundOffset = builder.backgroundOffset;
+        this._characterProps = builder.characterProps;
+    }
+
+    get backgroundInfo() {
+        return this._backgroundInfo;
+    }
+
+    get backgroundOffset() {
+        return this._backgroundOffset;
+    }
+
+    get characterProps() {
+        return this._characterProps;
+    }
+
+    /**
+     * Set the direction the background is moving. Saves to the parent component state.
+     *
+     * @param dir the direction the main character is moving
+     */
+    setBackgroundDirection(dir) {
+        if (this.backgroundInfo['backgroundInfo'] === undefined) {
+            this.backgroundInfo['backgroundInfo'] = {'pps' : 0, 'direction': dir === LEFT ? RIGHT : LEFT};
+        }
+        this.backgroundInfo['backgroundInfo']['direction'] = dir === LEFT ? RIGHT : LEFT;
+        this.backgroundInfo.setBackgroundDetail(this.backgroundInfo.backgroundInfo);
+    }
+
+    /**
+     * Sets the pixels per second of the background dictated by the pixels per second of the main characters current
+     * action. Performs a noop of the character is not the main character (the character with clicks bound).
+     *
+     * @param dir the direction of the character used to set the default direction
+     * @param pps the pixels per second of the character
+     */
+    setBackgroundPps(dir, pps) {
+        if (this.backgroundInfo['backgroundInfo'] === undefined) {
+            this.backgroundInfo['backgroundInfo'] = {'pps' : 0, 'direction': dir === LEFT ? RIGHT : LEFT};
+        }
+        this.backgroundInfo['backgroundInfo']['pps'] = pps;
+        this.backgroundInfo.setBackgroundDetail(this.backgroundInfo.backgroundInfo);
+    }
+
+    /**
+     * Stops the background animation.
+     */
+    stopBackgroundAnimation() {
+        this.backgroundOffset.setValue(this.backgroundOffset._value);
+    }
+
+    /**
+     * In order to support looped background scrolling there are three copies of the background image (left, center and
+     * right). If the character has moved right of the center of the background and is facing right the left background
+     * image copy is loaded so that when the character advances right the edge of the screen is not shown (instead the
+     * center copy is advanced into). Similarly if the character has moved left of center and is facing left the right
+     * background image copy is used. If neither of these conditions are met the character can move without the risk
+     * of the edge of the three images being visible.
+     *
+     * @param direction the direction the character is moving
+     * @returns {number} the new offset based on the character's direction and the current backround position.
+     */
+    getOffsetForDirection(direction) {
+
+        if (direction === RIGHT && this.backgroundOffset._value < -1*BACKGROUND_WIDTH_PX) {
+            return this.backgroundOffset._value + BACKGROUND_WIDTH_PX;
+        } else if (direction === LEFT && this.backgroundOffset._value > -1*BACKGROUND_WIDTH_PX) {
+            return this.backgroundOffset._value - BACKGROUND_WIDTH_PX;
+        } else {
+            return this.backgroundOffset._value;
+        }
+    }
+
+    /**
+     * Animate the background to give the illusion that the character is moving horizontally.
+     *
+     * @param action the action the character is currently performing
+     * @param direction the direction the character is currently pointing
+     */
+    animateBackground(action, direction) {
+
+        this.stopBackgroundAnimation();
+        if (!this.characterProps.bindClicks || !SCROLLING_ACTIONS.includes(action)) {
+            return;
+        }
+
+        let characterActionAnimationConfig = this.characterProps.characterAnimationConfig[direction][action];
+
+        let toValue = 0;
+        let distance = 0;
+        if (direction === RIGHT) {
+            //console.log('moving right from value %s to value is %s', backgroundOffset._value, -2668);
+            toValue = this.backgroundOffset._value - BACKGROUND_WIDTH_PX;
+            toValue = -2668;
+            distance = Math.abs(this.backgroundOffset._value - toValue);
+        } else {
+            //console.log('moving left from value %s and to value is %s', backgroundOffset._value, 0);
+            toValue = this.backgroundOffset._value + BACKGROUND_WIDTH_PX;
+            toValue = 0;
+            distance = Math.abs(this.backgroundOffset._value - toValue);
+        }
+
+        if (this.characterProps.bindClicks) {
+
+            Animated.timing(
+                this.backgroundOffset,
+                {
+                    toValue: toValue,
+                    duration: distance / characterActionAnimationConfig[PPS] * 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: false
+                }
+            ).start(({ finished }) => {
+                //clearInterval(this.spriteAnimationId);
+
+                console.log('background animation done was it finished? %s', finished);
+
+                if (finished) {
+                    if (direction === RIGHT) {
+                        this.backgroundOffset.setValue(-1*BACKGROUND_WIDTH_PX);
+                        this.animateBackground(action, direction);
+                    } else {
+                        //backgroundOffset.setValue()
+                        this.backgroundOffset.setValue(-1*BACKGROUND_WIDTH_PX);
+                        this.animateBackground(action, direction);
+                    }
+                }
+            });
+        }
+
+    }
+}
+
+export class BackgroundAnimationBuilder {
+    constructor(backgroundInfo) {
+        this._backgroundInfo = backgroundInfo;
+    }
+
+    get backgroundInfo() {
+        return this._backgroundInfo;
+    }
+
+    get backgroundOffset() {
+        return this._backgroundOffset;
+    }
+
+    withBackgroundOffset(backgroundOffset) {
+        this._backgroundOffset = backgroundOffset;
+        return this;
+    }
+
+    get characterProps() {
+        return this._characterProps;
+    }
+
+    withCharacterProps(characterProps) {
+        this._characterProps = characterProps;
+        return this;
+    }
+
+    build() {
+        return new BackgroundAnimation(this);
+    }
+}
--- animation/CharacterAnimation.js
+++ animation/CharacterAnimation.js
@@ -1,5 +1,6 @@
 import {BACKGROUND_WIDTH_PX, FPS, LEFT, PPS, RIGHT, SCROLLING_ACTIONS, STOP, WALK} from "../util/constants";
 import {Animated, Dimensions, Easing} from "react-native";
+import {BackgroundAnimationBuilder} from "./BackgroundAnimation";
 
 export class CharacterAnimation {
     constructor(builder) {
@@ -20,6 +21,14 @@ export class CharacterAnimation {
         this._positions = builder.positions;
         this._screenHeight = builder.screenHeight;
         this._setScreenHeight = builder.setScreenHeight;
+        this._backgroundAnimation =
+            new BackgroundAnimationBuilder(builder.backgroundInfo)
+                .withCharacterProps(builder.character.props)
+                .withBackgroundOffset(builder.backgroundOffset).build();
+    }
+
+    get backgroundAnimation() {
+        return this._backgroundAnimation;
     }
 
     get character() {
@@ -34,20 +43,12 @@ export class CharacterAnimation {
         return this._targetY;
     }
 
-    get backgroundInfo() {
-        return this._backgroundInfo;
-    }
-
     get setFrameIndex() {
         return this._setFrameIndex;
     }
 
     get setCharacterActionAnimationConfig() {
         return this._setCharacterActionAnimationConfig;
-    }
-
-    get backgroundOffset() {
-        return this._backgroundOffset;
     }
 
     get spriteAnimationId() {
@@ -86,16 +87,6 @@ export class CharacterAnimation {
         return this._setScreenHeight;
     }
 
-    isHorizontalDirectionChanged(gestureX) {
-        return gestureX - this.character.props.spriteWidth / 2 < this.character.x._value && this.character.direction == RIGHT ||
-            gestureX - this.character.props.spriteWidth / 2 >= this.character.x._value && this.character.direction === LEFT;
-    }
-
-    isVerticalDirectionChanged(gestureY) {
-        return gestureY - this.character.props.spriteHeight / 2 < this.character.y._value && gestureY - this.character.props.spriteHeight / 2 >= this.targetY ||
-            gestureY - this.character.props.spriteHeight / 2 >= this.character.y._value && gestureY - this.character.props.spriteHeight / 2 <= this.targetY;
-    }
-
     /**
      * Get the default value of the x coordinate based on the direction the character is facing
      *
@@ -108,11 +99,41 @@ export class CharacterAnimation {
     }
 
     /**
-     * Determines of the character is changing direction to the test direction with the new click
-     * @param e the click event
-     * @param testDir the direction to test
-     * @returns {boolean} true if the character is changing direction to testDir, false otherwise.
+     * Handles possible direction change. If the character is changing direction the background image loaded needs to be
+     * changed to the appropriate copy (left, right or center) so that the edge of the 3 copies of the background images
+     * is not shown.
+     * @param e the press event
      */
+    handleDirectionChange(gestureX, gestureY) {
+        clearInterval(this.spriteAnimationId);
+        let dir = this.isChangingDirectionTo(gestureX, gestureY, LEFT)
+            ? LEFT
+            : this.isChangingDirectionTo(gestureX, gestureY, RIGHT) ? RIGHT : this.character.direction;
+
+        this.character.setDirection(dir);
+
+        if (this.character.props.bindClicks) {
+            this.backgroundAnimation.setBackgroundDirection(dir);
+        }
+    }
+
+    /* private */
+    isHorizontalDirectionChanged(gestureX) {
+        return gestureX - this.character.props.spriteWidth / 2
+            < this.character.x._value && this.character.direction == RIGHT ||
+            gestureX - this.character.props.spriteWidth / 2
+                >= this.character.x._value && this.character.direction === LEFT;
+    }
+
+    /* private */
+    isVerticalDirectionChanged(gestureY) {
+        return gestureY - this.character.props.spriteHeight / 2
+            < this.character.y._value && gestureY - this.character.props.spriteHeight / 2 >= this.targetY ||
+            gestureY - this.character.props.spriteHeight / 2
+                >= this.character.y._value && gestureY - this.character.props.spriteHeight / 2 <= this.targetY;
+    }
+
+    /* private */
     isChangingDirectionTo(pageX, pageY, testDir) {
         let xValue = this.character.x._value !== undefined ? this.character.x._value : CharacterAnimation.getDefaultX(this.character.props);
         return testDir === LEFT
@@ -120,154 +141,13 @@ export class CharacterAnimation {
             : this.character.direction === LEFT && pageX >= xValue + this.character.props.spriteWidth / 2;
     }
 
-    /**
-     * Set the direction the background is moving. Saves to the parent component state.
-     *
-     * @param dir the direction the main character is moving
-     */
-    setBackgroundDirection(dir) {
-        if (this.character.props.bindClicks) {
-
-            if (this.backgroundInfo['backgroundInfo'] === undefined) {
-                this.backgroundInfo['backgroundInfo'] = {'pps' : 0, 'direction': dir === LEFT ? RIGHT : LEFT};
-            }
-            this.backgroundInfo['backgroundInfo']['direction'] = dir === LEFT ? RIGHT : LEFT;
-            this.backgroundInfo.setBackgroundDetail(this.backgroundInfo.backgroundInfo);
-        }
-    }
-
-    /**
-     * Handles possible direction change. If the character is changing direction the background image loaded needs to be
-     * changed to the appropriate copy (left, right or center) so that the edge of the 3 copies of the background images
-     * is not shown.
-     * @param e the press event
-     */
-    handleDirectionChange(pageX, pageY) {
-        clearInterval(this.spriteAnimationId);
-        let dir = this.isChangingDirectionTo(pageX, pageY, LEFT)
-            ? LEFT
-            : this.isChangingDirectionTo(pageX, pageY, RIGHT) ? RIGHT : this.character.direction;
-
-        this.character.setDirection(dir);
-
-        this.setBackgroundDirection(dir);
-    }
-
-    /**
-     * Stops the character movement
-     */
+    /* private */
     stopCharacterMovement() {
         if (this.character.x !== undefined) {
             Animated.timing(this.character.x, {useNativeDriver: false}).stop();
         }
         if (this.character.y != undefined) {
             Animated.timing(this.character.y, {useNativeDriver: false}).stop();
-        }
-
-    }
-
-    /**
-     * Sets the pixels per second of the background dictated by the pixels per second of the main characters current
-     * action. Performs a noop of the character is not the main character (the character with clicks bound).
-     *
-     * @param dir the direction of the character used to set the default direction
-     * @param pps the pixels per second of the character
-     */
-    setBackgroundPps(dir, pps) {
-        if (this.character.props.bindClicks) {
-            if (this.backgroundInfo['backgroundInfo'] === undefined) {
-                this.backgroundInfo['backgroundInfo'] = {'pps' : 0, 'direction': dir === LEFT ? RIGHT : LEFT};
-            }
-            this.backgroundInfo['backgroundInfo']['pps'] = pps;
-            this.backgroundInfo.setBackgroundDetail(this.backgroundInfo.backgroundInfo);
-        }
-    }
-
-    /**
-     * Stops the background animation.
-     */
-    stopBackgroundAnimation() {
-        this.backgroundOffset.setValue(this.backgroundOffset._value);
-    }
-
-    /**
-     * In order to support looped background scrolling there are three copies of the background image (left, center and
-     * right). If the character has moved right of the center of the background and is facing right the left background
-     * image copy is loaded so that when the character advances right the edge of the screen is not shown (instead the
-     * center copy is advanced into). Similarly if the character has moved left of center and is facing left the right
-     * background image copy is used. If neither of these conditions are met the character can move without the risk
-     * of the edge of the three images being visible.
-     *
-     * @param direction the direction the character is moving
-     * @returns {number} the new offset based on the character's direction and the current backround position.
-     */
-    getOffsetForDirection(direction) {
-
-        if (direction === RIGHT && this.backgroundOffset._value < -1*BACKGROUND_WIDTH_PX) {
-            return this.backgroundOffset._value + BACKGROUND_WIDTH_PX;
-        } else if (direction === LEFT && this.backgroundOffset._value > -1*BACKGROUND_WIDTH_PX) {
-            return this.backgroundOffset._value - BACKGROUND_WIDTH_PX;
-        } else {
-            return this.backgroundOffset._value;
-        }
-    }
-
-    /**
-     * Animate the background to give the illusion that the character is moving horizontally.
-     *
-     * @param action the action the character is currently performing
-     * @param direction the direction the character is currently pointing
-     */
-    animateBackground(action, direction) {
-
-        this.stopBackgroundAnimation();
-        if (!this.character.props.bindClicks || !SCROLLING_ACTIONS.includes(action)) {
-            return;
-        }
-
-        let characterActionAnimationConfig = this.character.props.characterAnimationConfig[direction][action];
-
-        let toValue = 0;
-        let distance = 0;
-        if (direction === RIGHT) {
-            //console.log('moving right from value %s to value is %s', backgroundOffset._value, -2668);
-            toValue = this.backgroundOffset._value - BACKGROUND_WIDTH_PX;
-            toValue = -2668;
-            distance = Math.abs(this.backgroundOffset._value - toValue);
-        } else {
-            //console.log('moving left from value %s and to value is %s', backgroundOffset._value, 0);
-            toValue = this.backgroundOffset._value + BACKGROUND_WIDTH_PX;
-            toValue = 0;
-            distance = Math.abs(this.backgroundOffset._value - toValue);
-        }
-
-        if (this.character.props.bindClicks) {
-
-            Animated.timing(
-                this.backgroundOffset,
-                {
-                    toValue: toValue,
-                    duration: distance / characterActionAnimationConfig[PPS] * 1000,
-                    easing: Easing.linear,
-                    useNativeDriver: false
-                }
-            ).start(({ finished }) => {
-                console.log('done animating at %s, setting offset to %s', this.backgroundOffset._value, this.getOffsetForDirection(direction));
-                clearInterval(this.spriteAnimationId);
-
-                console.log('background animation done was it finished? %s', finished);
-
-                if (finished) {
-                    if (direction === RIGHT) {
-                        this.backgroundOffset.setValue(-1*BACKGROUND_WIDTH_PX);
-                        this.animateBackground(action, direction);
-                    } else {
-                        //backgroundOffset.setValue()
-                        this.backgroundOffset.setValue(-1*BACKGROUND_WIDTH_PX);
-                        this.animateBackground(action, direction);
-                    }
-                }
-            });
         }
 
     }
@@ -301,7 +181,7 @@ export class CharacterAnimation {
         let index = 0;
         let characterActionAnimationConfig = this.character.props.characterAnimationConfig[dir][act];
         let timeout = characterActionAnimationConfig[FPS] === 0 ? 0 : 1000 / characterActionAnimationConfig[FPS];
-        let initialBackgroundPps = this.backgroundInfo['backgroundInfo'][PPS] === undefined ? 0 : this.backgroundInfo['backgroundInfo'][PPS];
+        let initialBackgroundPps = this.backgroundAnimation.backgroundInfo['backgroundInfo'][PPS] === undefined ? 0 : this.backgroundAnimation.backgroundInfo['backgroundInfo'][PPS];
         let animationId = setInterval(() => {
 
             if (index > characterActionAnimationConfig['offsets'].length - 1) {
@@ -315,10 +195,10 @@ export class CharacterAnimation {
             this.recordPosition();
 
             if (!this.character.props.bindClicks) {
-                let backgroundPps = this.backgroundInfo['backgroundInfo'][PPS] === undefined ? 0 : this.backgroundInfo['backgroundInfo'][PPS];
+                let backgroundPps = this.backgroundAnimation.backgroundInfo['backgroundInfo'][PPS] === undefined ? 0 : this.backgroundAnimation.backgroundInfo['backgroundInfo'][PPS];
                 if (backgroundPps != initialBackgroundPps) {
                     // If background is moving in the same direction of the character add to the pps, otherwise subtract
-                    let adjusted = backgroundPps * (dir !== this.backgroundInfo['backgroundInfo']['direction'] ? -1 : 1);
+                    let adjusted = backgroundPps * (dir !== this.backgroundAnimation.backgroundInfo['backgroundInfo']['direction'] ? -1 : 1);
 
                     // Stop movement and sprite animation and restart it with the adjusted pps
                     clearInterval(animationId);
@@ -426,9 +306,11 @@ export class CharacterAnimation {
         let characterActionAnimationConfig = this.character.props.characterAnimationConfig[dir][act];
 
 
-        this.setBackgroundPps(dir, characterActionAnimationConfig[PPS]);
+        if (this.character.props.bindClicks) {
+            this.backgroundAnimation.setBackgroundPps(dir, characterActionAnimationConfig[PPS]);
+        }
 
-        this.animateBackground(act, dir);
+        this.backgroundAnimation.animateBackground(act, dir);
         let spriteAnimationId = this.animateCharacterSprite(toX, toY, act, dir);
 
 
